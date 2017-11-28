b12455b707f3e720173f0b74ce3f49921697db78
Matt Davis <madavis111@outlook.com>
2017-11-27 16:21:00 -0800

    Change remaining <a> tags into <Link> components

--- app/src/Home.js
+++ app/src/Home.js
@@ -6,10 +6,10 @@ class Home extends React.Component {
     return (
       <div>
         <Link to="/add-words" className="button btn-large">Add new word</Link>
-    		<Link to="#/translate" className="button btn-large">Translate</Link>
-    	  <Link to="#/dictionary" className="button btn-large">Dictionary</Link>
-    	  <Link to="#/flash-cards" className="button btn-large">Flash cards</Link>
-    	  <Link to="#/notes" className="button btn-large">Notes</Link>
+    		<Link to="/translate" className="button btn-large">Translate</Link>
+    	  <Link to="/dictionary" className="button btn-large">Dictionary</Link>
+    	  <Link to="/flash-cards" className="button btn-large">Flash cards</Link>
+    	  <Link to="/notes" className="button btn-large">Notes</Link>
       </div>
     );
   }
