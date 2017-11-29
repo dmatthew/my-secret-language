7f0725e5a877fecdf56cf367959753fa013a7baf
Matt Davis <madavis111@outlook.com>
2017-11-28 17:11:11 -0800

    Create search functionality on dictionary page

--- app/src/Dictionary.js
+++ app/src/Dictionary.js
@@ -4,30 +4,54 @@ class Dictionary extends React.Component {
   constructor() {
     super();
     this.state = {
-      currentLetter: ''
+      currentLetter: '',
+      searchTerm: ''
     };
+  }
+
+  sortByMainWord(arr) {
+    arr.sort(function(a, b){
+      var valueA = a.mainWord.toLowerCase(), valueB = b.mainWord.toLowerCase();
+      if (valueA < valueB) return -1;
+      if (valueA > valueB) return 1;
+      return 0;
+    });
+    return arr;
+  }
+
+  handleSearchUpdate(event) {
+    event.preventDefault();
+    this.setState({searchTerm: event.target.value});
   }
 
   render() {
     let words = '';
     if (this.props.words) {
-      words = this.props.words.map((word, index) => {
-        return (
-          <span key={index}>
-            {word.mainWord.substr(0, 1) !== this.state.currentLetter &&
-              <li className="divider">{word.mainWord.substr(0, 1).toUpperCase()}</li>
-            }
-            <li>
-              <a>{word.mainWord}<span className="right">{word.secretWord}</span></a>
-            </li>
-          </span>
-        );
+      let searchTerm = this.state.searchTerm;
+      words = this.props.words.slice(0);
+      words = this.sortByMainWord(words);
+      words = words.map((word, index) => {
+        let found = word.mainWord.indexOf(searchTerm) !== -1 || word.secretWord.indexOf(searchTerm) !== -1;
+        if (searchTerm === '' || found) {
+          return (
+            <span key={index}>
+              {word.mainWord.substr(0, 1) !== this.state.currentLetter &&
+                <li className="divider">{word.mainWord.substr(0, 1).toUpperCase()}</li>
+              }
+              <li>
+                <a>{word.mainWord}<span className="right">{word.secretWord}</span></a>
+              </li>
+            </span>
+          );
+        }
+        else return undefined;
       });
     }
 
     return (
       <div>
-        <input type="search" placeholder="Enter search term..." />
+        <input value={this.state.searchTerm} onChange={(e) => this.handleSearchUpdate(e)}
+          type="search" placeholder="Enter search term..." />
         <ul>
         {words}
         </ul>
