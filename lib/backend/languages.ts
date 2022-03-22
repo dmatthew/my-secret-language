99310ec9ee6a233f9abeb3fb82c5e8ace54b0d24
Matt Davis <8683207+dmatthew@users.noreply.github.com>
2022-03-21 17:15:32 -0700

    Fix missing ssl config for PostgreSQL connections

--- lib/backend/languages.ts
+++ lib/backend/languages.ts
@@ -1,12 +1,17 @@
 import { Client } from 'pg'
 
 function getClient() {
+  const sslConfig =
+    process.env.NODE_ENV === 'development'
+      ? undefined //SSL not currently configured in local development
+      : { rejectUnauthorized: false }
   return new Client({
     user: process.env.DATABASE_USER,
     host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
     password: process.env.DATABASE_PASS,
     port: parseInt(process.env.DATABASE_PORT),
+    ssl: sslConfig,
   })
 }
 
