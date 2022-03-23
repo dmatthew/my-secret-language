9b3e05e16e66d44ca57d45acd9ce8239c6526f7e
Matt Davis <8683207+dmatthew@users.noreply.github.com>
2022-03-22 21:23:59 -0700

    Add missing dependency

--- lib/useLanguage.ts
+++ lib/useLanguage.ts
@@ -1,9 +1,9 @@
 import useSWR from 'swr'
-import { Language } from 'lib/types'
+import { LanguageType } from 'lib/types'
 import fetchJson from './fetchJson'
 
 export default function useLanguage(languageId: string) {
-  const { data: language, mutate: mutateLanguage } = useSWR<Language>(
+  const { data: language, mutate: mutateLanguage } = useSWR<LanguageType>(
     languageId ? `/api/languages/${languageId}` : null,
     () =>
       fetchJson(`/api/languages/${languageId}`, {
